97f9589005e36e58916942a81fb98716d7e1070f
blfuentes <epiphonetodie@gmail.com>
2019-12-06 14:01:16 +0100

    Day 6 F# file structure

--- FSharp/AoC_2019/Program.fs
+++ FSharp/AoC_2019/Program.fs
@@ -5,34 +5,40 @@ open System
 [<EntryPoint>]
 let main argv =
     // DAY 01
-    let resultday01Part1 = day01_part01.displayValue
-    printfn "Final result Day 01 part 1: %i" resultday01Part1
-    let resultday02Part2 = day01_part02.displayValue
-    printfn "Final result Day 01 part 2: %i" resultday02Part2
+    //let resultday01Part1 = day01_part01.displayValue
+    //printfn "Final result Day 01 part 1: %i" resultday01Part1
+    //let resultday02Part2 = day01_part02.displayValue
+    //printfn "Final result Day 01 part 2: %i" resultday02Part2
 
     // DAY 02
-    let resultday02Part1 = day02_part01.execute
-    printfn "Final result Day 02 part 1: %i" resultday02Part1
-    let resultday02Part2 = day02_part02.execute
-    printfn "Final result Day 02 part 2: %i" resultday02Part2
+    //let resultday02Part1 = day02_part01.execute
+    //printfn "Final result Day 02 part 1: %i" resultday02Part1
+    //let resultday02Part2 = day02_part02.execute
+    //printfn "Final result Day 02 part 2: %i" resultday02Part2
 
     // DAY 03
-    let resultday03Part1 = day03_part01.execute
-    printfn "Final result Day 03 part 1: %i" resultday03Part1
-    let resultday03Part2 = day03_part02.execute
-    printfn "Final result Day 03 part 2: %i" resultday03Part2
+    //let resultday03Part1 = day03_part01.execute
+    //printfn "Final result Day 03 part 1: %i" resultday03Part1
+    //let resultday03Part2 = day03_part02.execute
+    //printfn "Final result Day 03 part 2: %i" resultday03Part2
 
     // DAY 04
-    let resultday04Part1 = day04_part01.execute
-    printfn "Final result Day 04 part 1: %i" resultday04Part1
-    let resultday04Part2 = day04_part02.execute
-    printfn "Final result Day 04 part 2: %i" resultday04Part2
+    //let resultday04Part1 = day04_part01.execute
+    //printfn "Final result Day 04 part 1: %i" resultday04Part1
+    //let resultday04Part2 = day04_part02.execute
+    //printfn "Final result Day 04 part 2: %i" resultday04Part2
 
     // DAY 05
-    let resultday05Part1 = day05_part01.execute 1
-    printfn "Final result Day 05 part 1: %i" resultday05Part1
-    let resultday05Part2 = day05_part02.execute 5
-    printfn "Final result Day 05 part 2: %i" resultday05Part2
+    //let resultday05Part1 = day05_part01.execute 1
+    //printfn "Final result Day 05 part 1: %i" resultday05Part1
+    //let resultday05Part2 = day05_part02.execute 5
+    //printfn "Final result Day 05 part 2: %i" resultday05Part2
+
+    // DAY 06
+    let resultday06Part1 = day06_part01.execute
+    printfn "Final result Day 06 part 1: %i" resultday06Part1
+    let resultday06Part2 = day06_part02.execute
+    printfn "Final result Day 06 part 2: %i" resultday06Part2
 
     //
     let endprogram = Console.ReadLine()
---
+++ FSharp/AoC_2019/day06/part01/day06_part01.fsx
@@ -0,0 +1,10 @@
+﻿open System.IO
+
+//let filepath = __SOURCE_DIRECTORY__ + @"../../day06_input.txt"
+let filepath = __SOURCE_DIRECTORY__ + @"../../test_input.txt"
+let values = File.ReadAllLines(filepath)
+                |> Array.map (fun x -> (x.Split(')').[0], x.Split(')').[1]))
+
+type Orbit =
+    | Parent of string * Orbit list
+    | Child of string
