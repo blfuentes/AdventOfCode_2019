54b231a0b5e5198ca68345b776fac93e9631dbac
blfuentes <insanespeech@hotmail.es>
2019-12-08 09:05:39 +0100

    Day 7 part 2 on working

--- FSharp/AoC_2019/Modules/IntcodeComputerModule.fs
+++ FSharp/AoC_2019/Modules/IntcodeComputerModule.fs
@@ -22,7 +22,7 @@ module IntcodeComputerModule =
                 | (1, 1) -> (values.[idx + 1], values.[idx + 2])
                 | (_, _) -> (0, 0)
             Array.set values values.[idx + 3] (fst parameters + snd parameters)
-            (true, [|idx + 4; currentOutput; input|])
+            ((false, true), [|idx + 4; currentOutput; input|])
         | 2 -> // MULTIPLY
             let parameters = 
                 match (param1Mode, param2Mode) with
@@ -32,20 +32,21 @@ module IntcodeComputerModule =
                 | (1, 1) -> (values.[idx + 1], values.[idx + 2])
                 | (_, _) -> (0, 0)
             Array.set values values.[idx + 3] (fst parameters * snd parameters)
-            (true, [|idx + 4; currentOutput; input|])
+            ((false, true), [|idx + 4; currentOutput; input|])
         | 3 -> // WRITE INPUT
             let parameters = 
                 match (param1Mode, param2Mode) with
                 | (0, 0) -> (values.[idx + 1], 0)
                 | (_, _) -> (0, 0)
             Array.set values (fst parameters) phase
-            (true, [|idx + 2; currentOutput; input|])
+            ((false, true), [|idx + 2; currentOutput; input|])
         | 4 -> // OUTPUT
             let parameters = 
                 match (param1Mode, param2Mode) with
                 | (0, 0) -> (values.[values.[idx + 1]], 0)
                 | (_, _) -> (0, 0)
-            (true, [|idx + 2; fst parameters; input|])
+            ((true, true), [|idx + 2; fst parameters; input|])
+            //((true, true), [|idx; fst parameters; input|])
         | 5 -> // JUMP IF TRUE
             let parameters = 
                 match (param1Mode, param2Mode) with
@@ -56,8 +57,8 @@ module IntcodeComputerModule =
                 | (_, _) -> (0, 0)
 
             match fst parameters with
-            | 0 -> (true, [|idx + 3; currentOutput; input|])
-            | _ -> (true, [|(snd parameters); currentOutput; input|]) //((snd parameters) - idx, true)
+            | 0 -> ((false, true), [|idx + 3; currentOutput; input|])
+            | _ -> ((false, true), [|(snd parameters); currentOutput; input|]) //((snd parameters) - idx, true)
         | 6 -> // JUMP IF FALSE
             let parameters = 
                 match (param1Mode, param2Mode) with
@@ -68,8 +69,8 @@ module IntcodeComputerModule =
                 | (_, _) -> (0, 0)
 
             match fst parameters with
-            | 0 -> (true, [|(snd parameters); currentOutput; input|])//((snd parameters) - idx, true)
-            | _ -> (true, [|idx + 3; currentOutput; input|]) 
+            | 0 -> ((false, true), [|(snd parameters); currentOutput; input|])//((snd parameters) - idx, true)
+            | _ -> ( (false, true), [|idx + 3; currentOutput; input|]) 
         | 7 -> // LESS THAN
             let parameters = 
                 match (param1Mode, param2Mode) with
@@ -81,7 +82,7 @@ module IntcodeComputerModule =
 
             if (fst parameters < snd parameters) then Array.set values values.[idx + 3] 1
             else Array.set values values.[idx + 3] 0
-            (true, [|idx + 4; currentOutput; input|]) 
+            ((false, true), [|idx + 4; currentOutput; input|]) 
         | 8 -> // EQUALS
             let parameters = 
                 match (param1Mode, param2Mode) with
@@ -93,9 +94,9 @@ module IntcodeComputerModule =
 
             if (fst parameters = snd parameters) then Array.set values values.[idx + 3] 1
             else Array.set values values.[idx + 3] 0
-            (true, [|idx + 4; currentOutput; input|]) 
-        | 99 -> (false, [|idx + 3; currentOutput; input|]) // (lastoutput, false)
-        | _ -> (true, [|idx; currentOutput; input|]) // (0, true)
+            ((false, true), [|idx + 4; currentOutput; input|]) 
+        | 99 -> ((false, false), [|idx + 3; currentOutput; input|]) // (lastoutput, false)
+        | _ -> ((false, true), [|idx; currentOutput; input|]) // (0, true)
 
 
     let rec getOutput(values: int array, phase:int, input:int, idx: int, currentOutput:int) =
@@ -103,8 +104,18 @@ module IntcodeComputerModule =
         let resultOp = performOperation(values, phase, input, idx, opDefinition, currentOutput)
 
         match resultOp with
-        | (true, result) -> getOutput(values, result.[2], input, result.[0], result.[1])
-        | (false, result) -> result.[1]
+        | ((_, true), result) -> getOutput(values, result.[2], input, result.[0], result.[1])
+        | ((_, false), result) -> result.[1]
+
+    let rec getOutputPhaseLoopMode(values: int array, phase:int, input:int, idx: int, currentOutput:int) =
+        let opDefinition = values.[idx].ToString().PadLeft(5, '0') |> Seq.toArray |> Array.map string 
+        let resultOp = performOperation(values, phase, input, idx, opDefinition, currentOutput)
+
+        match resultOp with
+        | ((false, false), result) -> (result.[1], (result.[0], false)) // no pausa + no continuar
+        | ((false, true), result) -> getOutputPhaseLoopMode(values, result.[2], input, result.[0], result.[1]) // no pausa + continue
+        | ((true, false), result) -> (result.[1], (result.[0], true)) // pausa + no continuar
+        | ((true, true), result) -> (result.[1], (result.[0], true)) // pausa + continuar
 
     let execute(path:string, input:int) =
         let values = getInput path
@@ -117,9 +128,5 @@ module IntcodeComputerModule =
         let idx = 0
         getOutput(values, phase, input, idx, 0)  
 
-    //let executeWithPhase(path: string, phase:int, input:int) =
-    //    let values = getInput path
-    //    let idx = 0
-    //    let phaseResult = performOperation(values, phase, idx, [|"0"; "0"; "0"; "0"; "3"|], 0)
-    //    let inputResult = performOperation(values, input, (snd phaseResult).[0], [|"0"; "0"; "0"; "0"; "3"|], 0)
-    //    getOutput(values, (snd inputResult).[1], (snd inputResult).[0], 0)  
+    let executeWithPhaseLoopMode(values, phase: int, idx:int, input:int) =        
+        getOutputPhaseLoopMode(values, phase, input,idx, 0)  
