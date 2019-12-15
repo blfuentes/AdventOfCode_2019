aacfbce76e27d7a61779f53e180f4b4e9edd756a
blfuentes <insanespeech@hotmail.es>
2019-12-15 21:10:53 +0100

    Day 14 F# on work

--- FSharp/AoC_2019/day14/part01/day14_part01.fsx
+++ FSharp/AoC_2019/day14/part01/day14_part01.fsx
@@ -1,1 +1,19 @@
-﻿
+﻿open System.IO
+
+let filepath = __SOURCE_DIRECTORY__ + @"../../test_input_00.txt"
+
+type reactElement = { amount: int; name: string }
+
+let extractTransformation(reaction: string) =
+    let operators = reaction.Split('=')
+    let componentsPart = operators.[0].Split(',')
+    let resultPart = operators.[1].Replace("> ", "").Split(' ')
+    let result = { amount = int(resultPart.[0]); name = resultPart.[1] }
+    let components = seq {
+        for comp in componentsPart do
+            yield { amount = int(comp.Trim().Split(' ').[0]); name = comp.Trim().Split(' ').[1] }
+    }
+    (components, result)
+
+let transformations = File.ReadAllLines(filepath) |> Seq.map extractTransformation
+
