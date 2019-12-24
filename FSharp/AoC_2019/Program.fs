967f78c30f660b5287a07d9a7666f586b4648792
blfuentes <insanespeech@hotmail.es>
2019-12-24 17:41:49 +0100

    File structure all days

--- FSharp/AoC_2019/Program.fs
+++ FSharp/AoC_2019/Program.fs
@@ -94,6 +94,66 @@ let main argv =
     let resultday15Part2 = day15_part02.execute
     printfn "Final result Day 15 part 2: %A" resultday15Part2
 
+    // DAY 16
+    //let resultday16Part1 = day16_part01.execute
+    //printfn "Final result Day 16 part 1: %A" resultday16Part1
+    //let resultday16Part2 = day16_part02.execute
+    //printfn "Final result Day 16 part 2: %A" resultday16Part2
+
+    // DAY 17
+    //let resultday17Part1 = day17_part01.execute
+    //printfn "Final result Day 17 part 1: %A" resultday17Part1
+    //let resultday17Part2 = day17_part02.execute
+    //printfn "Final result Day 17 part 2: %A" resultday17Part2
+
+    // DAY 18
+    //let resultday18Part1 = day18_part01.execute
+    //printfn "Final result Day 18 part 1: %A" resultday18Part1
+    //let resultday18Part2 = day18_part02.execute
+    //printfn "Final result Day 18 part 2: %A" resultday18Part2
+
+    // DAY 19
+    //let resultday19Part1 = day19_part01.execute
+    //printfn "Final result Day 19 part 1: %A" resultday19Part1
+    //let resultday19Part2 = day19_part02.execute
+    //printfn "Final result Day 19 part 2: %A" resultday19Part2
+
+    // DAY 20
+    //let resultday20Part1 = day20_part01.execute
+    //printfn "Final result Day 20 part 1: %A" resultday20Part1
+    //let resultday20Part2 = day20_part02.execute
+    //printfn "Final result Day 20 part 2: %A" resultday20Part2
+
+    // DAY 21
+    //let resultday21Part1 = day21_part01.execute
+    //printfn "Final result Day 21 part 1: %A" resultday21Part1
+    //let resultday21Part2 = day21_part02.execute
+    //printfn "Final result Day 21 part 2: %A" resultday21Part2
+
+    // DAY 22
+    //let resultday22Part1 = day22_part01.execute
+    //printfn "Final result Day 22 part 1: %A" resultday22Part1
+    //let resultday22Part2 = day22_part02.execute
+    //printfn "Final result Day 22 part 2: %A" resultday22Part2
+
+    // DAY 23
+    //let resultday23Part1 = day23_part01.execute
+    //printfn "Final result Day 23 part 1: %A" resultday23Part1
+    //let resultday23Part2 = day23_part02.execute
+    //printfn "Final result Day 23 part 2: %A" resultday23Part2
+
+    // DAY 24
+    //let resultday24Part1 = day24_part01.execute
+    //printfn "Final result Day 24 part 1: %A" resultday24Part1
+    //let resultday24Part2 = day24_part02.execute
+    //printfn "Final result Day 24 part 2: %A" resultday24Part2
+
+    // DAY 25
+    //let resultday25Part1 = day25_part01.execute
+    //printfn "Final result Day 25 part 1: %A" resultday25Part1
+    //let resultday25Part2 = day25_part02.execute
+    //printfn "Final result Day 25 part 2: %A" resultday25Part2
+
     //
     let endprogram = Console.ReadLine()
     0 // return an integer exit code
--- FSharp/AoC_2019/day15/part01/day15_part01.fsx
+++ FSharp/AoC_2019/day15/part01/day15_part01.fsx
@@ -1,1 +1,100 @@
-﻿
+﻿open System.IO
+open System.Collections.Generic
+
+#load @"../../Modules/IntcodeComputerModule.fs"
+open AoC_2019.Modules
+
+type StatusType = WALL | WAY | OXYGEN | NONE
+type MovementType = NORTH | SOUTH | WEST | EAST | NONE
+
+let getStatus(status: bigint) =
+    match int(status) with
+    | 0 -> StatusType.WALL
+    | 1 -> StatusType.WAY
+    | 2 -> StatusType.OXYGEN
+    | _ -> StatusType.NONE
+
+let getMovement(movement: bigint) =
+    match int(movement) with
+    | 0 -> MovementType.NORTH
+    | 1 -> MovementType.SOUTH
+    | 2 -> MovementType.WEST
+    | 3 -> MovementType.EAST
+    | _ -> MovementType.NONE
+
+let containsElement(theSeq: seq<int[]>, element: int[]) =
+    let found = theSeq |> Seq.tryFind (fun x -> x.[0] = element.[0] && x.[1] = element.[1])
+    found <> None
+
+let getPoint(currPoint: int[], mov: MovementType) =
+    let newPoint = 
+        match mov with
+        | NORTH -> [|currPoint.[0]; currPoint.[1] - 1|]
+        | SOUTH -> [|currPoint.[0]; currPoint.[1] + 1|]
+        | EAST -> [|currPoint.[0] + 1; currPoint.[1]|]
+        | WEST -> [|currPoint.[0] - 1; currPoint.[1]|]
+        | _ -> currPoint
+    newPoint
+
+let finBSFPath(values: Dictionary<bigint, bigint>, relativeBase: bigint, movInput:bigint, idx:bigint, numberOfInputs: bigint) = 
+    let startPoint = [|0; 0|]
+    let resultPath = new Queue<int[]>()
+    let visitedPoints: List<int[]> = new List<int[]>()
+    resultPath.Enqueue(startPoint)
+    let currentPoint = [|0; 0|]
+    let paramInputs = [|relativeBase; idx; numberOfInputs|]
+    let mutable continueLooping = true
+    while resultPath.Count > 0 && continueLooping do
+        let movInputs = [| 0I; 1I; 2I; 3I |]
+        let tmpPoint = resultPath.Dequeue()
+        Array.set currentPoint 0 tmpPoint.[0]
+        Array.set currentPoint 1 tmpPoint.[1]
+        if not(containsElement(visitedPoints, currentPoint)) then    
+            for mov in movInputs do
+                let (output, (idx, notfinished), relativeBase)  =  executeBigDataWithMemory(values, paramInputs.[0], paramInputs.[1], mov, paramInputs.[2])
+                match getStatus(output) with
+                | WALL -> 
+                    printfn "Found Wall at %A - %A" idx (getMovement(mov))
+                    ()
+                | WAY -> 
+                    Array.set paramInputs 0 relativeBase
+                    Array.set paramInputs 1 idx
+                    Array.set paramInputs 2 numberOfInputs
+                    printfn "Found WAY at %A - %A" idx (getMovement(mov))
+                    let visitedPoint = getPoint(currentPoint, getMovement(mov))
+                    visitedPoints.Add(visitedPoint)
+                    resultPath.Enqueue(visitedPoint)
+                | OXYGEN -> 
+                    printfn "Found Oxygen at %A - %A" idx (getMovement(mov))
+                    continueLooping <- false
+                    ()
+                | _ ->
+                    ()
+
+        else
+            ()
+
+let execute =
+    let filepath = __SOURCE_DIRECTORY__ + @"../../day15_input.txt"
+    let alloutputs = new List<bigint>()
+    let values = IntcodeComputerModule.getInputBigData filepath
+    
+    let relBase = 0I
+    let movInput = 0I
+    let idx = 0I
+    let numInputs = 1I
+
+    finBSFPath(values, relBase, movInput, idx, numInputs)
+
+    //let (score, nextMovInput, toContinue) = round(values, relBase, movInput, idx, numInputs)
+    //values.[0I] <- 2I
+    //let valuesArray = [|relBase; movInput|]
+    //let mutable continueLooping = false
+    //let mutable finalScore = 0
+    //while continueLooping do
+    //    let (score, nextInput, numberOfBlocks) = finBSFPath(values, relBase, movInput, idx, numInputs)
+    //    continueLooping <- numberOfBlocks
+    //    finalScore <- score
+    //    Array.set valuesArray 1 nextInput
+
+    //finalScore
