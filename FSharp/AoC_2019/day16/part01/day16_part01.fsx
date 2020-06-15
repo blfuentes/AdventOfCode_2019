b0cfe850d2228ca36a0e9fedbe0c809b033b7390
blfuentes <epiphonetodie@gmail.com>
2020-06-15 21:19:58 +0200

    working on generate pattern day 16 part 1

--- FSharp/AoC_2019/day16/part01/day16_part01.fsx
+++ FSharp/AoC_2019/day16/part01/day16_part01.fsx
@@ -4,10 +4,20 @@ let filepath = __SOURCE_DIRECTORY__ + @"../../test_input_00.txt"
 //let filepath = __SOURCE_DIRECTORY__ + @"../../day16_input.txt"
 //let filepath = __SOURCE_DIRECTORY__ + @"../../day16_input.txt"
 
-let input = File.ReadAllText(filepath).ToCharArray() |> Array.map (fun x -> System.Convert.ToInt32((string)x))
+let input = File.ReadAllText(filepath).ToCharArray() |> Array.map (string >> int) 
+
+let generatePattern(length: int, position: int, basePattern: int[]) =
+    let tmpPattern = seq { for x in [| 0 .. length - 1|] do yield basePattern.[x % basePattern.Length] } |> Seq.toArray
+    tmpPattern
+
+let calculateInput(input:int[], position: int, pattern:int[]) =
+    let compPattern = generatePattern(input.Length, position, pattern)
+    let tmpResult = Array.map2 (*) input compPattern |> Array.sum |> fun x -> x % 10
+    tmpResult
+
+let test = calculateInput([|9;8;7;6;5|], 0, [|1;2;3|]) 
 
 let convertInput(pattern:int[], position: int, inputLength: int) =
-    let replicated = 
-
-let calculateInput(input:int[], pattern:int[]) =
+    // let replicated = 
     0
+
